6a9bf7459afc7bd60233a14deddefa82953148d7
Roberto <rpombar@gmail.com>
2015-07-15 00:24:46 +0200

    random version 1

---
+++ random.js
@@ -0,0 +1,8 @@
+//math.random() devuelve numero aleatorio entre 0 y 1
+var numero=math.random()
+if (numero<=0.5) then{
+	console.log('\n' + numero+ ' Menor que 0,5 \n');
+}
+else{
+	console.log('\n' + numero+ ' Mayor que 0,5 \n');
+}
